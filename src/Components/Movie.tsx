bc37e24daeb413cae46754d48b1a09b647093b5a
Szymon Grzeszczuk <szymon3520@gmail.com>
2022-12-06 10:35:26 +0100

    first commit

---
+++ src/Components/Movie.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Movie(props: any){
+
+}
+
+function title(props:any){
+
+}
---
+++ src/Data/moviesData.tsx
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 0,
+        title: "Titanic",
+        rating: 7.3,
+        reviewCount: 342455,
+        genre: "dramat",
+        director: "James Cameron",
+
+    },
+    {
+        id: 0,
+        title: "Titanic",
+        rating: 7.3,
+        reviewCount: 342455,
+        genre: "dramat",
+        director: "James Cameron",
+
+    },
+    {
+        id: 0,
+        title: "Zielona Mila",
+        altTitle: "The Green Mile",
+        rating: 7.3,
+        reviewCount: 342455,
+        genre: "dramat",
+        director: "James Cameron",
+
+    },
+
+];
